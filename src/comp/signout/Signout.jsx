9bc625990c2879e525f71fcd00ae3951a6c46a11
mihaicc30 <alemihai25@gmail.com>
2023-06-08 09:30:48 +0100

    signin update

--- src/comp/signout/Signout.jsx
+++ src/comp/signout/Signout.jsx
@@ -1,19 +1,37 @@
 import React, { useEffect } from "react";
 import "./Signout.css";
 import { useNavigate } from "react-router-dom";
+import { FiLoader } from "react-icons/fi";
 
-const Signout = ({ user, setUser }) => {
-	const nav = useNavigate();
-	useEffect(() => {
-		setUser(null);
-		nav("/");
-	}, []);
-	return (
-		<div className="basis-[80%] bg-[--c60] z-10 overflow-y-scroll">
-			Signout
-			{/* to have some animation of signing out */}
-		</div>
-	);
+const Signout = ({ setVenueNtable, setUser }) => {
+  const nav = useNavigate();
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setVenueNtable({ venue: null, table: null })
+      setUser(null);
+      nav("/");
+    }, 1000);
+
+    return () => clearTimeout(timeout); 
+  }, []);
+
+  return (
+    <div className="absolute inset-0 grow bg-[--c60] z-10 overflow-y-scroll flex flex-col justify-center items-center">
+      <div className="ui-loader loader-blk">
+        <svg viewBox="22 22 44 44" className="multiColor-loader">
+          <circle
+            cx="44"
+            cy="44"
+            r="20.2"
+            fill="none"
+            strokeWidth="3.6"
+            className="loader-circle loader-circle-animation"
+          ></circle>
+        </svg>
+      </div>
+      <p className="mt-8 text-xl">Logging user out...</p>
+    </div>
+  );
 };
 
 export default Signout;
